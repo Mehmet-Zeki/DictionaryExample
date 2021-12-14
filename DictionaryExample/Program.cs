f4ecd8f7a418ebd69d146fb70882a8dc3293c3bf
ZEKI OZDEN <mehmetzekiozden@hotmail.com>
2021-12-14 16:51:51 +0300

    Add project files.

---
+++ DictionaryExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace DictionaryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DictionaryList<string> bilgiler = new DictionaryList<string>();
+            bilgiler.Add("Zeki");
+            bilgiler.Add("Mehmet");
+            bilgiler.Print();
+            Console.WriteLine(bilgiler.Count);
+        }
+    }
+}
